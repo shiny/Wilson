7018d303d410fdd2f120f7e60cf55fa57fb4081d
DaiJie <daijie@php.net>
2023-08-03 21:33:01 +0800

    chore: package configuration

---
+++ packages/HandyAcme/src/index.ts
@@ -0,0 +1,18 @@
+import Fetch from "./Fetch"
+import AcmeFetch from "./AcmeFetch"
+import AcmeResponse from "./AcmeResponse"
+import Key from "./Key"
+import Directory from "./Directory"
+import Account from "./Account"
+
+export {
+    Fetch,
+    AcmeFetch,
+    AcmeResponse,
+    Key,
+    Directory,
+    Account
+}
+export * from  "./Account"
+export * from "./ResultError"
+export default {}
