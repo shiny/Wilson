8e62000c702e0b5a07434a046cc71ec5905b4e5e
戴劼 <daijie@php.net>
2023-08-04 01:33:44 +0800

    feat: hot reload example

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { type Serve } from "bun"
+import { Directory } from 'handyacme'
+/**
+ * Entrance of this Application
+ * with hot reloading support
+ * @docs https://bun.sh/guides/http/hot
+ */
+export default {
+    async fetch(req) {
+        const directory = await Directory.from('LetsEncrypt', 'staging')
+        return new Response(JSON.stringify(directory.result), {
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    },
+  } satisfies Serve;
