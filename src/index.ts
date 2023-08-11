6da33a2ce0088f88c770298b689001f708e393de
DaiJie <daijie@php.net>
2023-08-11 22:24:03 +0800

    feat: an example of FileSystemRouter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { type Serve } from "bun"
-import { Directory } from 'handyacme'
+
+const router = new Bun.FileSystemRouter({
+    style: 'nextjs',
+    dir: import.meta.dir + '/controller',
+})
 /**
  * Entrance of this Application
  * with hot reloading support
  * @docs https://bun.sh/guides/http/hot
  */
 export default {
-    async fetch(req) {
-        const directory = await Directory.from('LetsEncrypt', 'staging')
-        return new Response(JSON.stringify(directory.result), {
-            headers: {
-                'content-type': 'application/json'
+    async fetch(request) {
+        const matched = router.match(request)
+        if (matched) {
+            const { default: Controller } = await import(matched.filePath)
+            const controller = new Controller
+            const method = request.method.toLowerCase()
+            if (method in controller) {
+                return new Response(await controller[method](request))
             }
+        }
+        return new Response('Not Found', {
+            status: 404
         })
     },
 } satisfies Serve;
