d354707f161f3a3d9b28b941f14d3102dd25b0a7
DaiJie <daijie@php.net>
2023-08-15 21:58:41 +0800

    refactor: remove JsonResponse

--- packages/@tealight.uk/Soda/Response/JsonResponse.ts
+++
@@ -1,8 +0,0 @@
-export default class JsonResponse extends Response{
-    constructor(body: number | string | object, options?: ConstructorParameters<typeof Response>[1]) {
-        options = options ?? {}
-        options.headers = new Headers(options?.headers ?? {})
-        options.headers.set('content-type', 'application/json')
-        super(JSON.stringify(body), options)
-    }
-}
